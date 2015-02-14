9c768ae308431c057aa179e8edf8a173a2518429
MichaelD! <michael@dragonspark.us>
2015-02-14 18:43:17 -0500

    Basic Solution showing Ambiguous Reference for x:Type

---
+++ Resharper.Xamarin.Forms.Xaml/Resharper.Xamarin.Forms.Xaml/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Resharper.Xamarin.Forms.Xaml
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
