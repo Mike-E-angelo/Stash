9bf154db6686eb1d956efe348bca0cf78d8fb997
MichaelD! <michael@dragonspark.us>
2015-03-06 15:44:21 -0500

    Markup Issues Project

---
+++ Xamarin.Forms.Markup/Xamarin.Forms.Markup.Windows/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Markup;
+
+namespace Xamarin.Forms.Markup.Windows
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Debug.Assert( ContentControl.Content is string );
+		}
+	}
+
+	public class StringExtension : MarkupExtension
+	{
+		public override object ProvideValue( IServiceProvider serviceProvider )
+		{
+			return "Hello World... MarkupExtensions own your bones.";
+		}
+	}
+
+	[ContentProperty( "SomeObject" )]
+	public class ContainerExtension : MarkupExtension
+	{
+		public object SomeObject { get; set; }
+		
+		public override object ProvideValue( IServiceProvider serviceProvider )
+		{
+			return SomeObject;
+		}
+	}
+}
---
+++ Xamarin.Forms.Markup/Xamarin.Forms.Markup/Xamarin.Forms.Markup.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.Markup.WinPhone.Resources;
+
+namespace Xamarin.Forms.Markup.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
