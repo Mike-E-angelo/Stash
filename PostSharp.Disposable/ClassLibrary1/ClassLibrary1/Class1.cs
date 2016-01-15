cf8ac6a8a581f3fd518e4019f6ae42b4e7c664b0
MichaelD! <michael@dragonspark.us>
2016-01-15 07:06:57 -0500

    Disposable

---
+++ PostSharp.Disposable/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+	public class Class1 : IDisposable
+	{
+		public void Dispose()
+		{
+			// Dispose( true );
+			// GC.SuppressFinalize( this );
+		}
+
+		protected virtual void Dispose( bool disposing )
+		{}
+	}
+}
