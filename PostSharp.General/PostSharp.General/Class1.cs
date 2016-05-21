8988bcfa2e00775fc7cff514b10f66300c8e72a9
MichaelD! <michael@dragonspark.us>
2016-05-21 03:40:06 -0400

    Validation Bug.

---
+++ PostSharp.General/PostSharp.General/Class1.cs
@@ -0,0 +1,33 @@
+﻿using PostSharp.Patterns.Contracts;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace PostSharp.General
+{
+	public class Class1
+	{
+		[Fact]
+		public void Throws()
+		{
+			Assert.Throws<ArgumentNullException>( () => { new TypeAdapter( null ); } );
+		}
+
+		public class TypeAdapter
+		{
+			public TypeAdapter( [Required] Type type ) : this( type, type.GetTypeInfo() ) {}
+
+			public TypeAdapter( [Required] TypeInfo info ) : this( info.AsType(), info ) {}
+
+			public TypeAdapter( [Required] Type type, [Required] TypeInfo info )
+			{
+				Type = type;
+				Info = info;
+			}
+
+			public Type Type { get; }
+
+			public TypeInfo Info { get; }
+		}
+	}
+}
