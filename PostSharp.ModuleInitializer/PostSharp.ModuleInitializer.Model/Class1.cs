be8265bf69f594c71a9f943641c43411186e5056
MichaelD! <michael@dragonspark.us>
2016-05-30 14:51:45 -0400

    ModuleInitalizer locking bug.

---
+++ PostSharp.ModuleInitializer/PostSharp.ModuleInitializer.Model/Class1.cs
@@ -0,0 +1,54 @@
+﻿using PostSharp.Patterns.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace PostSharp.ModuleInitializer.Model
+{
+	/*public static class Initializer
+	{
+		[ModuleInitializer( 0 )]
+		public static void Initialize()
+		{
+			
+		}
+	}*/
+
+	public class AutoDataAttribute : DataAttribute
+	{
+		public override IEnumerable<object[]> GetData( MethodInfo testMethod )
+		{
+			AssemblyInitializer.Instance.Run( testMethod.DeclaringType.Assembly );
+			return new List<object[]> { new [] { new object() } };
+		}
+	}
+
+
+	[Synchronized]
+	public class AssemblyInitializer //: CommandBase<Assembly>
+	{
+		public static AssemblyInitializer Instance { get; } = new AssemblyInitializer();
+
+		AssemblyInitializer() /*: base( Specification.Instance )*/ {}
+
+		public void Run( Assembly parameter )
+		{
+			// Thread.Sleep( 2000 );
+			parameter.GetModules().Select( module => module.ModuleHandle ).ToList().ForEach( System.Runtime.CompilerServices.RuntimeHelpers.RunModuleConstructor );
+			/*if ( !Activated( parameter ) )
+			{
+				parameter.Set( DragonSpark.TypeSystem.Activated.Property, true );
+			}*/
+		}
+
+		// static bool Activated( Assembly parameter ) => parameter.Get( DragonSpark.TypeSystem.Activated.Property );
+
+		/*class Specification : OncePerParameterSpecification<Assembly>
+		{
+			public static Specification Instance { get; } = new Specification();
+
+			public override bool IsSatisfiedBy( Assembly parameter ) => !Activated( parameter ) && base.IsSatisfiedBy( parameter );
+		}*/
+	}
+}
---
+++ PostSharp.ModuleInitializer/PostSharp.ModuleInitializer/Class1.cs
@@ -0,0 +1,45 @@
+﻿using PostSharp.Aspects;
+using PostSharp.ModuleInitializer.Model;
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace PostSharp.ModuleInitializer
+{
+	public class Class1
+	{
+		[Fact]
+		public void Test()
+		{
+			if ( !Initializer.Initialized )
+			{
+				throw new InvalidOperationException( "Should not be here." );
+			}
+		}
+	}
+
+	public class Class2
+	{
+		[Theory, AutoData]
+		public void Test( object parameter )
+		{
+			if ( !Initializer.Initialized )
+			{
+				throw new InvalidOperationException( "Should not be here." );
+			}
+		}
+	}
+	
+	public static class Initializer
+	{
+		public static bool Initialized { get; private set; }
+
+		[ModuleInitializer( 0 )]
+		public static void Initialize()
+		{
+			Trace.WriteLine( "Initializing" );
+			AssemblyInitializer.Instance.Run( typeof(object).Assembly );
+			Initialized = true;
+		}
+	}
+}
