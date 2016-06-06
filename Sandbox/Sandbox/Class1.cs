1ecefaa6ee08aa931791a4af7e1982cc29abb8eb
MichaelD! <michael@dragonspark.us>
2016-06-06 17:30:56 -0400

    dotMemory allocations.

---
+++ Sandbox/Sandbox/Class1.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.dotMemoryUnit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sandbox
+{
+	public class Class1
+	{
+		public Class1( ITestOutputHelper output )
+		{
+			DotMemoryUnitTestOutput.SetOutputMethod( output.WriteLine );
+		}
+
+		[Fact]
+		[DotMemoryUnit( SavingStrategy = SavingStrategy.OnCheckFail, Directory = @"C:\dotMemory", FailIfRunWithoutSupport = false )]
+		[AssertTraffic( AllocatedObjectsCount = 0 )]
+		public void BasicTest()
+		{
+			
+		}
+
+		[Fact]
+		[DotMemoryUnit( SavingStrategy = SavingStrategy.OnCheckFail, Directory = @"C:\dotMemory", FailIfRunWithoutSupport = false )]
+		[AssertTraffic( AllocatedObjectsCount = 0 )]
+		public void BasicTest2()
+		{
+			
+		}
+	}
+}
