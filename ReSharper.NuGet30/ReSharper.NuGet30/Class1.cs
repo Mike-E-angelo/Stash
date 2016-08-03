1813a7a77c2e6545a64381fce46dc1a9f83c652e
MichaelD! <michael@dragonspark.us>
2016-08-03 12:27:03 -0400

    Adding Theory's that work.

--- ReSharper.NuGet30/ReSharper.NuGet30/Class1.cs
+++ ReSharper.NuGet30/ReSharper.NuGet30/Class1.cs
@@ -5,9 +5,26 @@ namespace ReSharper.NuGet30
 	public class Class1
 	{
 		[Theory, Ploeh.AutoFixture.Xunit2.AutoData]
-		public void TestingMethod()
+		public void TestingMethod( int sut )
 		{
 			Assert.True( true );
 		}
+
+		[Theory]
+		[InlineData( 3 )]
+		[InlineData( 5 )]
+		[InlineData( 6 )]
+		public void MyFirstTheory( int value )
+		{
+			Assert.True( IsOdd( value ) );
+		}
+
+		bool IsOdd( int value )
+		{
+			return value % 2 == 1;
+		}
+
+		[Fact]
+		public void Testing() {}
 	}
 }
