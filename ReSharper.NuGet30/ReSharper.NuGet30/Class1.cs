e0367d35f07642b7035af7ca071186bd75ebd551
MichaelD! <michael@dragonspark.us>
2016-08-03 11:29:03 -0400

    NuGet3.0 Breaking Theory test.

---
+++ ReSharper.NuGet30/ReSharper.NuGet30/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ReSharper.NuGet30
+{
+	public class Class1
+	{
+		[Theory, Ploeh.AutoFixture.Xunit2.AutoData]
+		public void TestingMethod()
+		{
+			Assert.True( true );
+		}
+	}
+}
