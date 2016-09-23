7f20bd5391441f3a9dd53ee5d599f4b926418f5b
MichaelD! <michael@dragonspark.us>
2016-09-23 05:39:17 -0400

    More ordering madness.

---
+++ PostSharp.AspectOrdering2/PostSharp.AspectOrdering2/Class1.cs
@@ -0,0 +1,57 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Aspects.Configuration;
+using PostSharp.Extensibility;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace PostSharp.AspectOrdering2
+{
+	public class Class1
+	{
+		[Fact]
+		public void Verify()
+		{
+			var sut = new Subject();
+		}
+	}
+
+	[Serializable, LinesOfCodeAvoided( 1 )]
+	class FirstAspect : TypeLevelAspect
+	{
+		public override void CompileTimeInitialize( Type type, AspectInfo aspectInfo )
+		{
+			MessageSource.MessageSink.Write( new Message( MessageLocation.Unknown, SeverityType.ImportantInfo, "6776", $"{this}: CompileTimeInitialize", null, null, null ));
+			Thread.Sleep( 250 );
+			base.CompileTimeInitialize( type, aspectInfo );
+		}
+
+		protected override void SetAspectConfiguration( AspectConfiguration aspectConfiguration, Type targetType )
+		{
+			base.SetAspectConfiguration( aspectConfiguration, targetType );
+			MessageSource.MessageSink.Write( new Message( MessageLocation.Unknown, SeverityType.ImportantInfo, "6776", $"{this}: SetAspectConfiguration", null, null, null ));
+			Thread.Sleep( 250 );
+		}
+	}
+
+	[Serializable, LinesOfCodeAvoided( 1 )/*, AspectTypeDependency( AspectDependencyAction.Order, AspectDependencyPosition.Before, typeof(FirstAspect) )*/]
+	class SecondAspect : TypeLevelAspect
+	{
+		public override void CompileTimeInitialize( Type type, AspectInfo aspectInfo )
+		{
+			MessageSource.MessageSink.Write( new Message( MessageLocation.Unknown, SeverityType.ImportantInfo, "6776", $"{this}: CompileTimeInitialize", null, null, null ));
+			Thread.Sleep( 250 );
+			base.CompileTimeInitialize( type, aspectInfo );
+		}
+
+		protected override void SetAspectConfiguration( AspectConfiguration aspectConfiguration, Type targetType )
+		{
+			base.SetAspectConfiguration( aspectConfiguration, targetType );
+			MessageSource.MessageSink.Write( new Message( MessageLocation.Unknown, SeverityType.ImportantInfo, "6776", $"{this}: SetAspectConfiguration", null, null, null ));
+			Thread.Sleep( 250 );
+		}
+	}
+
+	[SecondAspect, FirstAspect]
+	class Subject {}
+}
---
+++ PostSharp.AspectOrdering2/PostSharp.AspectOrdering2/SecondAspect.cs
@@ -0,0 +1,10 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Aspects.Configuration;
+using PostSharp.Extensibility;
+using System;
+using System.Threading;
+
+namespace PostSharp.AspectOrdering2
+{
+	
+}
