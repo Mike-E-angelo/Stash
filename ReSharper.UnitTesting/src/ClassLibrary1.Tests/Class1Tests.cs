f2fa6f5681b4e38ca3aebdd0a9ebd6dc3b1384cd
Mike-EEE <michael@dragonspark.us>
2017-02-10 07:51:04 -0500

    ReSharper UnitTesting issue.

---
+++ ReSharper.UnitTesting/src/ClassLibrary1.Tests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary1.Tests
+{
+	public class Class1Tests
+	{
+		public Class1Tests()
+		{
+		}
+
+		[Fact]
+		public void Testing()
+		{
+			Debugger.Break();
+		}
+	}
+}
