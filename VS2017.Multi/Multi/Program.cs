0d5d131a598903cdc14eee4059d62c895722d891
Mike-EEE <michael@dragonspark.us>
2017-06-18 12:57:44 -0400

    Multi-problem.

---
+++ VS2017.Multi/Multi/Program.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using System;
+
+namespace Multi
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var temp = new Class1();
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
