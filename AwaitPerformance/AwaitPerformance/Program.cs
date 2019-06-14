c6a2f54d69768b26e87e5f8132e63399fa874c45
Mike-EEE <michael@dragonspark.network>
2019-06-14 11:41:09 -0400

    AwaitPerformance

---
+++ AwaitPerformance/AwaitPerformance/Program.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AwaitPerformance
+{
+	public sealed class Program
+	{
+		static void Main()
+		{
+			BenchmarkRunner.Run<Benchmarks>();
+		}
+
+		[MemoryDiagnoser]
+		public class Benchmarks
+		{
+			[Benchmark]
+			public async Task<uint> AwaitList()
+			{
+				var list = new List<Task>();
+
+				for (var i = 0u; i < 10; i++)
+				{
+					list.Add(Task.Delay(1));
+				}
+
+				await Task.WhenAll(list).ConfigureAwait(false);
+
+				return 123;
+			}
+
+			[Benchmark]
+			public async Task<uint> AwaitEach()
+			{
+				for (var i = 0u; i < 10; i++)
+				{
+					await Task.Delay(1).ConfigureAwait(false);
+				}
+
+				return 123;
+			}
+		}
+	}
+}
