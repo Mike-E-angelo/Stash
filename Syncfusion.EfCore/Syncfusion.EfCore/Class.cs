ce690fe0744d16ecc77a395b20f2408b8f67047e
Mike-E <michael@dragonspark.network>
2021-01-17 08:11:27 -0500

    Syncfusion issue

---
+++ Syncfusion.EfCore/Syncfusion.EfCore/Class.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Syncfusion.EfCore
+{
+	public class SimpleContext : DbContext
+	{
+		public SimpleContext(DbContextOptions<SimpleContext> options) : base(options) {}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Simple>()
+			            .HasData(new Simple { Id = 1, Name = "Hello" }, new Simple { Id = 2, Name = "World" });
+		}
+
+		public DbSet<Simple> Items { get; set; }
+	}
+
+	public class Simple
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
