785188b61c57f0b7f036b8f5c07b902b412e797b
Mike-E <michael@dragonspark.network>
2021-08-04 18:57:37 -0400

    Hyperlinq.Memory

---
+++ Hyperlinq.Memory/Hyperlinq.Memory/CreateOwner.cs
@@ -0,0 +1,25 @@
+﻿using NetFabric.Hyperlinq;
+using System.Collections.Generic;
+
+namespace Hyperlinq.Memory
+{
+	public sealed class CreateOwner<T>
+	{
+		public static CreateOwner<T> Default { get; } = new CreateOwner<T>();
+
+		CreateOwner() {}
+
+		public Owner<T> Get(IEnumerable<T> parameter, int size = 4)
+		{
+			var result = new Owner<T>(size);
+			var count  = 0;
+			var span   = result.Memory.Span;
+			foreach (var i in parameter.AsValueEnumerable())
+			{
+				span[count++] = i;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Hyperlinq.Memory/Hyperlinq.Memory/Owner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Buffers;
+
+namespace Hyperlinq.Memory
+{
+	public readonly struct Owner<T> : IMemoryOwner<T>
+	{
+		readonly ArrayPool<T> _pool;
+
+		public Owner(int size) : this(ArrayPool<T>.Shared, size) {}
+
+		public Owner(ArrayPool<T> pool, int size) : this(pool, size, pool.Rent(size)) {}
+
+		public Owner(ArrayPool<T> pool, int size, T[] store) : this(pool, store, store.AsMemory()[..size]) {}
+
+		public Owner(ArrayPool<T> pool, T[] store, Memory<T> memory)
+		{
+			Store  = store;
+			Memory = memory;
+			_pool  = pool;
+		}
+
+		public T[] Store { get; }
+
+		public Memory<T> Memory { get; }
+
+
+
+		public void Dispose()
+		{
+			_pool.Return(Store);
+		}
+	}
+}
