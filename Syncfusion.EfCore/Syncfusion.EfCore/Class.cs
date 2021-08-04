5b8331a997ad26bea857cb44815358cb0ad5d025
Mike-E <michael@dragonspark.network>
2021-08-04 18:57:17 -0400

    Syncfusion edits

--- Syncfusion.EfCore/Syncfusion.EfCore/Class.cs
+++ Syncfusion.EfCore/Syncfusion.EfCore/Class.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Syncfusion.EfCore
 {
@@ -20,5 +24,53 @@ namespace Syncfusion.EfCore
 		public int Id { get; set; }
 
 		public string Name { get; set; }
+	}
+
+	// Implementing custom adaptor by extending the DataAdaptor class
+	public class CustomAdaptor : DataAdaptor
+	{
+		readonly IQueryable<Simple> _query;
+
+		public CustomAdaptor(SimpleContext context) : this(context.Items) {}
+
+		CustomAdaptor(IQueryable<Simple> context) => _query = context;
+
+		public override async Task<object> ReadAsync(DataManagerRequest dm, string key = null)
+		{
+			var query = _query;
+
+			if (dm.Search?.Count > 0)
+			{
+				query = DataOperations.PerformSearching(query, dm.Search);
+			}
+
+			if (dm.Sorted?.Count > 0)
+			{
+				// Sorting
+				query = DataOperations.PerformSorting(query, dm.Sorted);
+			}
+
+			if (dm.Where?.Count > 0)
+			{
+				// Filtering
+				query = DataOperations.PerformFiltering(query, dm.Where, dm.Where[0].Operator);
+			}
+
+			if (dm.Skip != 0)
+			{
+				//Paging
+				query = DataOperations.PerformSkip(query, dm.Skip);
+			}
+
+			if (dm.Take != 0)
+			{
+				query = DataOperations.PerformTake(query, dm.Take);
+			}
+
+			var result = dm.RequiresCounts
+				             ? new DataResult { Result = await query.ToListAsync(), Count = await query.CountAsync() }
+				             : (object)query;
+			return result;
+		}
 	}
 }
--- Syncfusion.EfCore/Syncfusion.EfCore/Startup.cs
+++ Syncfusion.EfCore/Syncfusion.EfCore/Startup.cs
@@ -37,6 +37,7 @@ namespace Syncfusion.EfCore
 				                   return result;
 			                   });
 			services.AddSyncfusionBlazor();
+			services.AddScoped<CustomAdaptor>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
