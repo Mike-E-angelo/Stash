96dfda1ff8970f39c6b500945e481c2a95ee2c98
Mike-E <michael@dragonspark.network>
2021-08-23 04:24:17 -0400

    Added compiled benchmarks

---
+++ EfCore.ScopedVsTransaction/EfCore.ScopedVsTransaction/Class1.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.ScopedVsTransaction
+{
+	[MemoryDiagnoser]
+	public class ProvidedBenchmarks
+	{
+		private BlogContext _reusableContext { get; set; }
+		private PooledDbContextFactory<BlogContext> _factory { get; set; }
+
+		[GlobalSetup]
+		public async Task Setup()
+		{
+			var options = new DbContextOptionsBuilder<BlogContext>().UseInMemoryDatabase("foo").Options;
+			_reusableContext = new BlogContext(options);
+			await _reusableContext.Database.EnsureDeletedAsync();
+			_factory = new PooledDbContextFactory<BlogContext>(options);
+		}
+
+		[Benchmark]
+		public Blog[] Same()
+		{
+			return _reusableContext.Blogs.AsNoTracking().ToArray();
+		}
+
+		[Benchmark]
+		public Blog[] Pooled()
+		{
+			using var context = _factory.CreateDbContext();
+			return context.Blogs.AsNoTracking().ToArray();
+		}
+
+		public class BlogContext : DbContext
+		{
+			public DbSet<Blog> Blogs { get; set; }
+			public BlogContext(DbContextOptions options) : base(options) {}
+		}
+
+		public class Blog
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		public enum Providers { SqlServer, InMemory }
+	}
+}
---
+++ EfCore.ScopedVsTransaction/EfCore.ScopedVsTransaction/CompiledBenchmarks.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.ScopedVsTransaction
+{
+	public class CompiledBenchmarks
+	{
+		readonly IDbContextFactory<Context>               _factory;
+		readonly Func<Context, IAsyncEnumerable<Subject>> _compile;
+		readonly IQueryable<Subject>                      _subjects;
+
+		public CompiledBenchmarks()
+			: this(new DbContextOptionsBuilder<Context>().UseInMemoryDatabase(new Guid().ToString()).Options) {}
+
+		public CompiledBenchmarks(DbContextOptions<Context> options)
+			: this(new PooledDbContextFactory<Context>(options),
+			       Select.Default.Get(new Context(options).Set<Subject>().AsNoTracking())) {}
+
+		public CompiledBenchmarks(IDbContextFactory<Context> factory, IQueryable<Subject> subjects)
+			: this(factory, EF.CompileAsyncQuery<Context, Subject>(x => x.Subjects.AsNoTracking()
+			                                                             .Where(y => y.Name != "Two")
+			                                                             .Where(y => y.Name != "Two")
+			                                                             .Where(y => y.Name != "Two")), subjects) {}
+
+		public CompiledBenchmarks(IDbContextFactory<Context> factory, Func<Context, IAsyncEnumerable<Subject>> compile,
+		                          IQueryable<Subject> subjects)
+		{
+			_factory  = factory;
+			_compile  = compile;
+			_subjects = subjects;
+		}
+
+		[Benchmark(Baseline = true)]
+		public Task<Subject[]> Scoped() => _subjects.ToArrayAsync();
+
+		[Benchmark]
+		public async Task<Subject[]> Pooled()
+		{
+			await using var context = _factory.CreateDbContext();
+			var             compile = await _compile(context).ToArrayAsync();
+			return compile;
+		}
+	}
+}
