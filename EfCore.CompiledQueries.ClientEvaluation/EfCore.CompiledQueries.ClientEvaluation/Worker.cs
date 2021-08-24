6b46e70315ef4a6eea74c8b3b001913186b0e3c7
Mike-E <michael@dragonspark.network>
2021-08-24 16:16:01 -0400

    Logging output

--- EfCore.CompiledQueries.ClientEvaluation/EfCore.CompiledQueries.ClientEvaluation/Worker.cs
+++ EfCore.CompiledQueries.ClientEvaluation/EfCore.CompiledQueries.ClientEvaluation/Worker.cs
@@ -1,6 +1,7 @@
 ﻿using EfCore.CompiledQueries.ClientEvaluation.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace EfCore.CompiledQueries.ClientEvaluation
 	sealed class Worker : IHostedService
 	{
 		readonly IDbContextFactory<Context> _contexts;
+		readonly ILogger<Worker>            _logger;
 
-		public Worker(IDbContextFactory<Context> contexts) => _contexts = contexts;
+		public Worker(IDbContextFactory<Context> contexts, ILogger<Worker> logger)
+		{
+			_contexts = contexts;
+			_logger   = logger;
+		}
 
 		public async Task StartAsync(CancellationToken cancellationToken)
 		{
@@ -18,12 +24,19 @@ namespace EfCore.CompiledQueries.ClientEvaluation
 			await context.Database.EnsureDeletedAsync();
 			if (await context.Database.EnsureCreatedAsync())
 			{
-				context.Subjects.AddRange(new Subject { Name = "One" }, new Subject { Name = "Two" });
+				context.Subjects.AddRange(new Subject { Name = "One" }, new Subject { Name = "Two" },
+				                          new Subject { Name = "Three" });
 				await context.SaveChangesAsync();
 			}
 
 			var evaluate = new SubjectsNotTwo(new DbContexts<Context>(_contexts));
-			await evaluate.Get(None.Default);
+			var items    = await evaluate.Get(None.Default);
+			_logger.LogInformation("There are {Count} Subjects.", items.Length);
+
+			foreach (var subject in items)
+			{
+				_logger.LogInformation("Subject Name: {Name}", subject.Name);
+			}
 		}
 
 		public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
