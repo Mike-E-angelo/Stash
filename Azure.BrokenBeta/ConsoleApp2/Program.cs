9772ab33f24a62b2e04bf2b31e2a0112df281898
Mike-E <michael@dragonspark.network>
2021-08-25 10:58:31 -0400

    https://github.com/Azure/azure-webjobs-sdk/issues/2753

---
+++ Azure.BrokenBeta/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static async Task Main()
+		{
+			var builder = new HostBuilder();
+			builder.UseEnvironment(EnvironmentName.Development);
+			builder.ConfigureWebJobs(b =>
+			                         {
+				                         b.AddAzureStorageCoreServices();
+			                         });
+			builder.ConfigureLogging((context, b) =>
+			                         {
+				                         b.AddConsole();
+			                         });
+			builder.ConfigureWebJobs(b =>
+			                         {
+				                         b.AddAzureStorageCoreServices();
+				                         b.AddAzureStorageQueues();
+			                         });
+			var host = builder.Build();
+			using (host)
+			{
+				await host.RunAsync();
+			}
+		}
+
+	}
+
+	public class Functions
+	{
+		public static void ProcessQueueMessage([QueueTrigger("queue-name")] string message, ILogger logger)
+		{
+			logger.LogInformation(message);
+		}
+	}
+}
