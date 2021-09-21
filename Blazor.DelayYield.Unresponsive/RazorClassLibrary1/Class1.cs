60b89f862a1b13d55be0ed183c6208489c96af85
Mike-E <michael@dragonspark.network>
2021-09-21 10:32:38 -0400

    Simplified model

--- Blazor.DelayYield.Unresponsive/RazorClassLibrary1/Class1.cs
+++ Blazor.DelayYield.Unresponsive/RazorClassLibrary1/Class1.cs
@@ -4,49 +4,14 @@ using System.Threading.Tasks;
 
 namespace RazorClassLibrary1
 {
-	public class Resulting<T> : Result<Task<T>>, IResulting<T>
+	public sealed class HelloWorld
 	{
-		public Resulting(IResult<Task<T>> result) : base(result) {}
+		readonly Func<Task<string?>> _source;
 
-		public Resulting(Func<Task<T>> source) : base(source) {}
+		public HelloWorld(Func<Task<string?>> source) => _source = source;
+
+		public Task<string?> Get() => _source();
 	}
-
-	public class Result<T> : IResult<T>
-	{
-		public static implicit operator T(Result<T> result) => result.Get();
-
-		readonly Func<T> _source;
-
-		public Result(IResult<T> result) : this(result.Get) {}
-
-		public Result(Func<T> source) => _source = source;
-
-		public T Get() => _source();
-	}
-
-
-	public class Instance<T> : IResulting<T>
-	{
-		readonly T _instance;
-
-		public Instance(T instance) => _instance = instance;
-
-		public Task<T> Get() => Task.FromResult(_instance);
-	}
-
-	public sealed class Defaulting<T> : Instance<T?>
-	{
-		public static Defaulting<T> Default { get; } = new();
-
-		Defaulting() : base(default) {}
-	}
-
-	public interface IResult<out T>
-	{
-		T Get();
-	}
-
-	public interface IResulting<T> : IResult<Task<T>> {}
 
 	public class ActiveContentTemplateComponentBase<T> : ContentTemplateComponentBase<T>
 	{
