8c97c10aa32342100c7e045db6f157762116465a
Mike-E <michael@dragonspark.network>
2021-12-07 15:58:20 -0500

    Initial commit

---
+++ Syncfusion.AdaptiveGrid/DataGrid_Server/Data/WeatherForecast.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace DataGrid_Server.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+    public class EmployeeData
+    {
+        public int? EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime? HireDate { get; set; }
+    }
+
+
+    public class Order
+    {
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public double? Freight { get; set; }
+    }
+}
---
+++ Syncfusion.AdaptiveGrid/DataGrid_Server/wwwroot/resized.js
@@ -0,0 +1,14 @@
+﻿var dotnetref;
+function resized(instance) {
+    dotnetref = instance;
+    Checksize();
+    window.onresize = function (e) {
+        Checksize();
+    };
+}
+function Checksize() {
+	const rect = document.getElementsByClassName("required-container")[0].getBoundingClientRect();
+	const width = rect.width;
+    const adaptive = width < 500;
+    dotnetref.invokeMethodAsync('Resized', adaptive ? "Mobile" : "Desktop");
+}
